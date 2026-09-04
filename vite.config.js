0c18967e349e9079edfaf369ccc0e6bcba1c4151
Vineeth2005 <Vineethreddy802@gmail.com>
2026-09-04 13:56:21 +0530

    payment backend updated

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/payments': {
-        target: 'http://127.0.0.1:8092',
+        target: 'http://127.0.0.1:3314',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/payments/, ''),
       },
